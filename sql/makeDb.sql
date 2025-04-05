9e7cac3a461d1f920d5472d6c04f0ba4333b2c5a
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-04 18:00:13 -0600

    updated db schema

--- sql/makeDb.sql
+++ sql/makeDb.sql
@@ -7,19 +7,87 @@ DROP TABLE IF EXISTS PTTrips;
 CREATE TABLE PTTrips (
     tripId SERIAL PRIMARY KEY,
     driverEmail VARCHAR(255) NOT NULL,
+    carrier VARCHAR(255) NOT NULL,
+    carrierAddress VARCHAR(255) NOT NULL,
+    inspectionAddress VARCHAR(255) NOT NULL,
+    make VARCHAR(100) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    odometer INT NOT NULL,
     truckPlate VARCHAR(20),
     trailerPlateA VARCHAR(20),
     trailerPlateB VARCHAR(20),
     date TIMESTAMP NOT NULL,
-    defects TEXT
+    defects text
 );
-
---Dummy data use your email
-INSERT INTO PTTrips (tripId, driverEmail, truckPlate, trailerPlateA, trailerPlateB, date, defects)
+INSERT INTO
+    PTTrips (
+        driverEmail,
+        carrier,
+        carrierAddress,
+        inspectionAddress,
+        make,
+        model,
+        odometer,
+        truckPlate,
+        trailerPlateA,
+        trailerPlateB,
+        date,
+        defects
+    )
 VALUES 
-    (1, 'patrick@patmac.ca', 'AXA6969', 'TRL123', '', '2025-04-01 09:30:00', '["Low tire pressure", "Broken headlight"]'),
-    (2, 'patrick@patmac.ca', 'AXA6969', 'TRL124', '', '2025-04-01 09:30:00', '["Oil leak", "Cracked windshield"]'),
-    (3, 'patrick@patmac.ca', 'AXA6969', 'TRL125', '', '2025-04-01 09:30:00', '["Faulty brake light", "Worn brake pads"]'),
-    (4, 'patrick@patmac.ca', 'AXA6969', 'TRL126', '', '2024-02-25 16:45:00', '["Missing mirror", "Damaged mudflap"]');
-
-
+    (
+        'patrick@patmac.ca',
+        'ABC Trucking',
+        '123 Truck St, City, ST',
+        '456 Inspection Ave, City, ST',
+        'Peterbilt',
+        '579',
+        '150000',
+        'AXA6969',
+        'TRL123',
+        'TRLB-1234',
+        '2025-04-01 09:30:00',
+        NULL
+    ),
+    (
+        'patrick@patmac.ca',
+        'ABC Trucking',
+        '123 Truck St, City, ST',
+        '456 Inspection Ave, City, ST',
+        'Peterbilt',
+        '579',
+        '150500',
+        'AXA6969',
+        'TRL124',
+        NULL,
+        '2025-04-01 09:30:00',
+        NULL
+    ),
+    (
+        'patrick@patmac.ca',
+        'ABC Trucking',
+        '123 Truck St, City, ST',
+        '456 Inspection Ave, City, ST',
+        'Peterbilt',
+        '579',
+        '151000',
+        'AXA6969',
+        'TRL125',
+        NULL,
+        '2025-04-01 09:30:00',
+        NULL
+    ),
+    (
+        'patrick@patmac.ca',
+        'ABC Trucking',
+        '123 Truck St, City, ST',
+        '456 Inspection Ave, City, ST',
+        'Peterbilt',
+        '579',
+        '151500',
+        'AXA6969',
+        'TRL126',
+        NULL,
+        '2024-02-25 16:45:00',
+        NULL
+    );
