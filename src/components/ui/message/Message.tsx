24a774c397904bd1d169c2eb3e4122974af698fb
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-04 20:53:14 -0600

    added back link to message component

--- src/components/ui/message/Message.tsx
+++ src/components/ui/message/Message.tsx
@@ -1,6 +1,13 @@
-export default function Message({ message }: { message: string }) {
+import Link
+    from "next/link"
+export default function Message({ message, driverEmail }: { message: string, driverEmail?: string }) {
     return (
         <div className="w-full md:w-4/6 p-8 gap-8 border rounded-sm">
+            {driverEmail &&
+                <div className="my-4">
+                    <Link href={`/pretrips/${driverEmail}`}>Back to Trips</Link>
+                </div>
+            }
             <h1 className="text-2xl">{message}</h1>
         </div>
     )
