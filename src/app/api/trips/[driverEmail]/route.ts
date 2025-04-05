deca55f8a0d2cc17144aa19c8aa753c02d3f53eb
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-04 18:00:42 -0600

    added uri decode to trips

--- src/app/api/trips/[driverEmail]/route.ts
+++ src/app/api/trips/[driverEmail]/route.ts
@@ -5,11 +5,11 @@ import { NextRequest } from 'next/server';
 export async function GET(request: NextRequest) {
     const url = new URL(request.url);
     const pathSegments = url.pathname.split('/');
-    const userEmail = pathSegments[pathSegments.length - 1];
-    const uriDecodedUserEmail = decodeURIComponent(userEmail);
+    const driverEmail = pathSegments[pathSegments.length - 1];
+    const uriDecodedDriverEmail = decodeURIComponent(driverEmail);
     try {
         const sql = neon(`${process.env.DATABASE_URL}`);
-        const result = await sql`SELECT * FROM PTTrips WHERE driverEmail = ${uriDecodedUserEmail} AND date >= CURRENT_DATE - INTERVAL '15 days' ORDER BY date`;
+        const result = await sql`SELECT * FROM PTTrips WHERE driverEmail = ${uriDecodedDriverEmail} AND date >= CURRENT_DATE - INTERVAL '15 days' ORDER BY date`;
         return new Response(JSON.stringify(result as Trip[]), { headers: { 'Content-Type': 'application/json' } });
     } catch (error) {
         return new Response(`Error getting comments: ${error}`, { headers: { 'Content-Type': 'text/plain' } });
