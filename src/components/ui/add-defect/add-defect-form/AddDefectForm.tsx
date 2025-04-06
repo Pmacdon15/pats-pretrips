ce0ad74c5e3ac8f93506abaff18ae063b8b374f6
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-06 15:17:36 -0600

    added error message

--- src/components/ui/add-defect/add-defect-form/AddDefectForm.tsx
+++ src/components/ui/add-defect/add-defect-form/AddDefectForm.tsx
@@ -1,8 +1,9 @@
 import { AddDefect } from "@/components/ui/add-defect/AddDefect"
 
-export default function AddDefectForm({ tripId, driverEmail, isError, isPendingChange, formAction }: {
+export default function AddDefectForm({ tripId, driverEmail, error, isError, isPendingChange, formAction }: {
     tripId: number;
     driverEmail: string;
+    error: Error | null;
     isError: boolean;
     isPendingChange: boolean;
     formAction: ({ driverEmail, tripId, formData }: { driverEmail: string; tripId: number; formData: FormData; }) => void;
@@ -12,7 +13,7 @@ export default function AddDefectForm({ tripId, driverEmail, isError, isPendingC
             action={(formData: FormData) => formAction({ driverEmail, tripId, formData })}
             className='flex flex-col gap-4 w-full '>
             <AddDefect requiered={true} />
-            {isError && <div className="text-center text-red-600">Error Adding Deffect</div>}
+            {isError && <div className="text-center text-red-600">Error Adding Deffect: {error?.message} </div>}
             <div className="flex justify-center w-full">
                 <button disabled={isPendingChange} className={`border p-4 rounded-lg bg-green-400 hover:bg-green-600 w-full md:w-3/6`}>Add on Route Defects</button>
             </div>
