a6d8dd258b50216d4e12a8ec6840192cb61f198e
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-06 12:44:07 -0600

    update css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen gap-8 p-4  ">
-      
-      <div className="flex flex-col border items-center justify-center rounded-md w-full md:w-3/6  p-8 shadow-md gap-4">
+
+      <div className="flex flex-col border items-center justify-center rounded-md w-full md:w-4/6  p-8 shadow-md gap-4">
         <p className="text-sm  indent-8 border rounded-sm p-8 shadow-md">
           Currently, only GitHub, GitLab login is available. GitHub/GitLab/Discord accounts are free - feel free to sign up to try the app!
-          Built with Next.js 15, Shadcn UI, TanStack Query, Tailwind CSS, and TypeScript.
+          Built with Next.js 15, TanStack Query, Tailwind CSS, NextAuth and TypeScript.
         </p>
         <AuthButtons session={session} />
       </div>
