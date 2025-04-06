916899c4b810b3805880278bce96a5154161e930
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-05 23:18:41 -0600

    update css for auth buttons

--- src/components/ui/auth-buttons/auth-buttons.tsx
+++ src/components/ui/auth-buttons/auth-buttons.tsx
@@ -11,22 +11,22 @@ interface Props {
 export default function Authbutton({ session }: Props) {
     return (
         <>
-            <div className='flex flex-wrap p-4 gap-4'>
+            <div className='flex flex-wrap p-4 gap-4 '>
                 {!session ? (
                     <div className='flex flex-col md:flex-row gap-4 p-4'>
-                        <button className='border p-4 rounded-sm shadow-lg' onClick={() => loginGoogle()}>
+                        <button className='flex items-center border p-4 rounded-sm shadow-lg' onClick={() => loginGoogle()}>
                             <FontAwesomeIcon icon={faGoogle} className="mr-2" />
                             Sign In With Google
                         </button >
-                        <button className='border p-4 rounded-sm shadow-lg' onClick={() => loginGithub()}>
+                        <button className='flex items-center border p-4 rounded-sm shadow-lg' onClick={() => loginGithub()}>
                             <FontAwesomeIcon icon={faGithub} className="mr-2" />
                             Sign In With GitHub
                         </button >                        
-                        <button className='border p-4 rounded-sm shadow-lg' onClick={() => loginGitlab()}>
+                        <button className='flex items-center border p-4 rounded-sm shadow-lg' onClick={() => loginGitlab()}>
                             <FontAwesomeIcon icon={faGitlab} className="mr-2" />
                             Sign In With GitLab
                         </button >
-                        <button className='border p-4 rounded-sm shadow-lg' onClick={() => loginDiscord()}>
+                        <button className='flex items-center border p-4 rounded-sm shadow-lg' onClick={() => loginDiscord()}>
                             <FontAwesomeIcon icon={faDiscord} className="mr-2" />
                             Sign In With Discord
                         </button >
