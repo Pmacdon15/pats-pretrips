c545b5cd56ee692d2e05626be615102aa0f4ad17
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-07 00:47:11 -0600

    revert

--- src/auth.ts
+++ src/auth.ts
@@ -6,20 +6,6 @@ import Discord from 'next-auth/providers/discord';
 import Twitter from 'next-auth/providers/twitter';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [
-    Google,
-    GitHub,
-    GitLab,
-    Discord,
-    Twitter({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: 'users.read email',
-        },
-      },
-    }),
-  ],
-  trustHost: true,
+    providers: [Google, GitHub, GitLab, Discord, Twitter],
+    trustHost: true,
 })
