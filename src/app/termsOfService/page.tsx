23ea9a0badc409b52eea930737bb7b3a653fba5c
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-06 21:10:35 -0600

    updated tos

--- src/app/termsOfService/page.tsx
+++ src/app/termsOfService/page.tsx
@@ -6,11 +6,11 @@ export default function Page() {
             <div className="flex flex-col border items-center justify-center rounded-md w-5/6 md:w-3/6 p-8 shadow-md gap-4">
                 <h1>Terms of Service</h1>
                 <p className="text-sm indent-8 border rounded-sm p-8 shadow-md">
-                    Pat&apos;s Pre-Trips is currently in development and testing phase. Please be aware that during this period,
+                    Pat&apos;s Pre-Trips is currently in production but please be aware that during this period,
                     data security and stability cannot be guaranteed. Any data you enter into the application may be lost,
-                    modified, or compromised during testing and development activities. By using this application, you
+                    modified, or compromised from time to time. By using this application, you
                     acknowledge these risks and agree to use the service at your own risk. We recommend not storing any
-                    sensitive or critical information until the full release version is available.
+                    sensitive or critical information until the full release version is available. At this time we offer the service free.
                 </p>
                 <GoHomeButton />
             </div>
