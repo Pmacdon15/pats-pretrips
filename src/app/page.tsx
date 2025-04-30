84eee9c9997361801439fa9de1b212cc1892e8e8
Patrick MacDonald <pmacdonald15@gmail.com>
2025-04-29 19:01:02 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     <div className="flex flex-col  items-center justify-items-center gap-4 p-4  ">
       <div className="flex flex-col border bg-[var(--color-primary)] items-center justify-center rounded-md w-full md:w-4/6  p-4 shadow-md gap-4 ">
         <p className="text-sm  indent-8 border bg-[var(--color-background)] rounded-sm p-8 shadow-md">
-          Currently, only GitHub, GitLab login is available. GitHub/GitLab/Discord accounts are free - feel free to sign up to try the app!
+          Currently, only login through one of the below services is available. Each service provides free accounts - feel free to sign up to try the app!
           Built with Next.js 15, TanStack Query, Tailwind CSS, NextAuth and TypeScript.
         </p>
         <p className='text-center p-4'>
