7c5af8c5f826f10a9d4eaf3920c379943a1678e0
Patrick MacDonald <patrick@patmac.ca>
2025-11-13 22:27:20 -0700

    updated fallback

--- src/app/pretrip/[tripId]/loading.tsx
+++ src/app/pretrip/[tripId]/loading.tsx
@@ -1,3 +1,9 @@
+import DisplayTripFallback from '@/lib/components/ui/fallbacks/display-trip-falback'
+
 export default function Loading() {
-	return <h1>Loading ...</h1>
+	return (
+		<div className="flex flex-col items-center justify-items-center gap-4 p-4">
+			<DisplayTripFallback />
+		</div>
+	)
 }
