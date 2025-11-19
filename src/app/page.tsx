457910483e95df9ee148ec112400c78807a67626
Patrick MacDonald <patrick@patmac.ca>
2025-11-18 17:07:04 -0700

    updated words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
 		<div className="flex flex-col items-center justify-items-center gap-4 p-4">
 			<div className="flex w-full flex-col items-center justify-center gap-4 rounded-md bg-[var(--color-primary)] p-4 shadow-md md:w-4/6">
 				<p className="rounded-sm bg-[var(--color-background)] p-8 indent-8 text-sm shadow-md">
-					Currently, only login through one of the below services is
-					available. Accounts with these services are free - feel free
-					to sign up to try the app! Built with Next.js 15, TanStack
-					Query, Tailwind CSS, NextAuth and TypeScript.
+					Currently, only login through google or email is
+					available. Feel free
+					to sign up to try the app! Built with Next.js 16, TanStack
+					Query, Tailwind CSS, Kinde Auth and TypeScript.
 				</p>
 				<p className="p-4 text-center">
 					Checkout our{' '}
